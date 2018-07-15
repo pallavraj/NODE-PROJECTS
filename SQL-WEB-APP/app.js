2f0d4991724a849869d738b4c073ec0c807bdcee
pallav <pallavraj40@gmail.com>
2018-07-15 23:58:50 +0530

    2nd Commit

--- SQL-WEB-APP/app.js
+++ SQL-WEB-APP/app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+app.set("view engine", "ejs");
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "pallav",
+    password : "pallav",
+    database : "join_us"
+
+});
+
 
 
 app.get("/",function(re1,res) {
-    res.send("HELLO");
+    var q = "select count(*) AS count from users";
+    connection.query(q , function (err , results) {
+    if(err) throw err;
+    var count = results[0].count;
+    });
+
+    res.render("home");
+
 });
+
+app.get("/joke", function (req , res) {
+
+    res.render("home");
+})
 
 app.listen(5000,function () {
 
