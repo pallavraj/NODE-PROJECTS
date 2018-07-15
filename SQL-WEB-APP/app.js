d0175450cd9277f632fb37aed5b25f2753565b50
pallav <pallavraj40@gmail.com>
2018-07-16 01:09:47 +0530

    COMPLETED

--- SQL-WEB-APP/app.js
+++ SQL-WEB-APP/app.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bodyParser = require("body-parser");
+
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
     host : "localhost",
     user : "pallav",
-    password : "pallav",
+    password : "******",
     database : "join_us"
 
 });
 
+app.post("/register", function (req,res) {
 
+var email = req.body.email;
+var person ={
+    email : req.body.email,
+};
+
+connection.query("insert into users set ?",person ,function (err ,results) {
+
+    if(err) throw err;
+
+    res.redirect("/");
+
+});
+
+
+});
 
 app.get("/",function(re1,res) {
     var q = "select count(*) AS count from users";
     connection.query(q , function (err , results) {
     if(err) throw err;
     var count = results[0].count;
+        res.render("home",{data : count});
     });
-
-    res.render("home");
 
 });
 
